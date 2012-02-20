cbe16a297f91fb73968680b222c4616860221847
cheinzmann3@gmail.com <cheinzmann3@gmail.com@f4396f2e-32fd-5734-d85a-65116df3bbda>
2012-02-20 06:13:00 +0000

    added more end-functional robot code.  Driving works, shooting works, bridge tipping, and rollers
    UNTESTED CODE

--- trunk/Teleop.cpp
+++ trunk/Teleop.cpp
@@ -13,6 +13,26 @@ void Coriolis::Teleop(void)
 	robotDrive.Drive(left.GetY(), right.GetY());
 	END_REGION
 	
+	CRITICAL_REGION(semaphore)
+	// set shooter turns on if left trigger pressed
+	robotShooter.SetShootPower(1.0);
+	robotShooter.EnableShooter(left.GetTrigger());
+	
+	// intake balls and shoot if right trigger pressed
+	if (right.GetTrigger())
+	{
+		robotShooter.IntakeBalls();
+		
+		// if shooter is sped up, shoot
+		if (robotShooter.IsShooterOn())
+			robotShooter.Shoot();
+	}
+	
+	// if right button #3 pressed, toggle bridge tipper
+	if (right.GetRawButton(3))
+		bridgeTipper.Move();
+	END_REGION
+	
 	/* Lo/Hi gear handling. */
 	CRITICAL_REGION(semaphore)
 	if (left.GetTrigger())
